e4a71dcee291fc25fc46896b543f54923007b700
Taofeek Olusola <olusolataofeek@gmail.com>
2024-02-26 15:02:38 +0100

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,29 +1,6 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-class flask (
-  String $version = '2.1.0',
-) {
-
-  package { 'python3-pip':
-    ensure => present,
-  }
-
-  package { 'virtualenv':
-    ensure => present,
-  }
-
-  exec { 'create_virtualenv':
-    command => "python3 -m virtualenv /path/to/your/virtualenv",
-    creates => "/path/to/your/virtualenv",
-    require => [
-      Package['python3-pip'],
-      Package['virtualenv'],
-    ],
-  }
-
-  exec { 'install_flask':
-    command => "/path/to/your/virtualenv/bin/pip3 install flask==${version}",
-    require => Exec['create_virtualenv'],
-  }
-
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
