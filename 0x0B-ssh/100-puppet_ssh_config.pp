0379c8bc671d78527a1c63aa524a0e617135d2b7
Taofeek Olusola <olusolataofeek@gmail.com>
2024-02-28 13:48:03 +0100

    0x0B-ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+#/usr/bin/env bash
+
+	Host myserver
+                HostName 54.197.44.130
+                User ubuntu
+                IdentityFile ~/.ssh/school
+                PasswordAuthentication no
