ae3f4664fae987b1283c05c52645c069c07439af
Taofeek Olusola <olusolataofeek@gmail.com>
2024-04-14 08:00:57 +0100

    0x17-web_stack_debugging_3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# Puppet manifest to change .phpp to .php in wp-settings.php
+
 file { '/var/www/html/wp-settings.php':
   ensure  => present,
   content => inline_template('<%= File.read("/var/www/html/wp-settings.php").gsub(/\.phpp/, ".php") %>'),
